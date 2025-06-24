1900328c08684f517ddc261c09c0333a036b4eeb
caffein.junkie <dev.satrio.ap@gmail.com>
2025-06-24 14:00:18 +0700

    allow browser

--- app/api/openai/route.ts
+++ app/api/openai/route.ts
@@ -4,6 +4,7 @@ import { getFunctions, getSystemPrompt } from "@/utils/prompts";
 
 const client = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
+  dangerouslyAllowBrowser: true
 });
 
 export async function POST(req: Request) {
